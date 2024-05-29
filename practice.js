a36ca71c77092e814bc2707cfeef10e7f3b2a376
kenyansa <kenyansajustine@gmail.com>
2024-05-29 13:17:57 +0300

    check days left to xmas

--- practice.js
+++ practice.js
@@ -70,3 +70,13 @@ function tuesdayYearlyCheck(){
     }
 }
 tuesdayYearlyCheck();
+
+//calculate days remaining before Christmas
+today = new Date ();
+let xmas = new Date(today.getFullYear(), 11, 25)
+if(today.getMonth() == 11 && today.getDate() > 25){
+    xmas.setFullYear(xmas.getFullYear() +1);
+}
+
+const oneDay = 1000 * 60 * 60 * 24;
+console.log(Math.ceil((xmas.getTime() - today.getTime()) / (oneDay)) + "days left until Christmas!");
