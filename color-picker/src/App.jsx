3762b3fdd8f48794ad68224ee4ed2fe30bfc91e3
kenyansa <kenyansajustine@gmail.com>
2024-05-31 20:29:07 +0300

    some styling

--- color-picker/src/App.jsx
+++ color-picker/src/App.jsx
@@ -1,18 +1,13 @@
-import { useState } from 'react'
-import './index.css';
+import ColorPicker from './ColorPicker';
+import './App.css';
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">React State</span> with a Color Picker.</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <h1 className="flex justify-center mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">React State</span> with a Color Picker.</h1>
+      <ColorPicker />
     </>
   )
 }
