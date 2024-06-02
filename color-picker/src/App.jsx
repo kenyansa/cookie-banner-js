84eee46a1a2a5693ab691ed7e18d4df220d112fc
kenyansa <kenyansajustine@gmail.com>
2024-06-02 11:42:23 +0300

    updating objects in state

--- color-picker/src/App.jsx
+++ color-picker/src/App.jsx
@@ -1,5 +1,6 @@
 import ColorPicker from './ColorPicker';
 import './App.css';
+import NewComponent from './NewComponent';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <h1 className="flex justify-center mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">React State </span> with a Color Picker.</h1>
       <ColorPicker />
+      <NewComponent />
     </>
   )
 }
